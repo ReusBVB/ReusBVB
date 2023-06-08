f53570f8b258ba60f0b880456016958c538e2916
ReusBVB <130775968+ReusBVB@users.noreply.github.com>
2023-06-08 08:00:28 +0300

    Add files via upload

---
+++ 5pr/2zd/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BookPages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Нахождение общего числа страниц во всех журналах. ");
+            int totalPages = 0;
+            int nextPageCount;
+
+            do
+            {
+                Console.Write("Введите количество страниц следующего издания: ");
+                nextPageCount = int.Parse(Console.ReadLine());
+
+                if (nextPageCount > 16)
+                {
+                    totalPages += nextPageCount;
+                }
+            }
+            while (nextPageCount != 0);
+
+            Console.WriteLine($"Количество страниц: {totalPages}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 5pr/5pr/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EvenNegativeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Вывод отрицательных чётных чисел в обратном порядке в промежутке от A до B. ");
+            Console.Write("Введите A: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите B: ");
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = b; i >= a; i--)
+            {
+                if (i < 0 && i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
