fa9025d6cfb3c5232dd7a8e81a9dafc9971bab01
ReusBVB <130775968+ReusBVB@users.noreply.github.com>
2023-06-08 05:15:46 +0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ax^2 + bx + c = 0");
+            Console.WriteLine("Введите значение a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение c");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double d = (b * b) - (4 * a * c);
+            if (d < 0) { Console.WriteLine("Корней нет"); }
+            else if (d == 0)
+            {
+                double x = (-b) / 2 * a;
+                Console.WriteLine("x = " + x);
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(d)) / 2 * a;
+                double x2 = (-b - Math.Sqrt(d)) / 2 * a;
+                Console.WriteLine("x1 = " + x1);
+                Console.WriteLine("x2 = " + x2);
+            }
+            Console.WriteLine("Для завершения нажми Enter");
+            Console.Read();
+        }
+    }
+}
