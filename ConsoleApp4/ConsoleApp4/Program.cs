59a5f68f4e23bc092d2b51fab1509fd6f7281e3d
ReusBVB <130775968+ReusBVB@users.noreply.github.com>
2023-06-08 05:23:48 +0300

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Введите стартовый капитал");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = x + (x / 100 + 3) + (x / 100 + 8);
+            Console.WriteLine(y);
+
+
+        }
+
+
+    }
+
+
+
+
+
+
+}
