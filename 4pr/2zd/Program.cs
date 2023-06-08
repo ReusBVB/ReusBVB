3cd135825fc5fa90ed699d3f0a39d9e5874c7476
ReusBVB <130775968+ReusBVB@users.noreply.github.com>
2023-06-08 07:59:34 +0300

    Add files via upload

---
+++ 4pr/2zd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BacteriaCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("На сколько размножились бактерии");
+            int bacteriaCount = 1;
+            uint minutes;
+
+            Console.WriteLine("Введите количество минут");
+            minutes = uint.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= minutes; i++)
+            {
+                bacteriaCount *= 2;
+            }
+
+            Console.WriteLine($"Количество бактерий: {bacteriaCount}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 4pr/4pr/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _4pr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Нахождение суммы вещественных чисел");
+            Console.Write("Введите N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            double sum = 1;
+            double k = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                k = sum / i + k;
+                Console.WriteLine($"k = {k}");
+            }
+
+            Console.WriteLine($"Сумма: {k}");
+            Console.ReadKey();
+        }
+    }
+}
